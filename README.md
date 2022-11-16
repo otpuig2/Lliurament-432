c27e7c64b62b3a087d5580d70703ebb4fbd17df8
Ot <1671762@uab.cat>
2022-11-16 21:04:50 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Part-A-equipo-432-13
---
+++ films.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+i=$i
+
+Opcions()
+echo "-------------------------------------------------------------------------">
+echo "                  1 - Llistats de pel·lícules."
+echo "-------------------------------------------------------------------------">
+echo "1.         Mostrar llistat de pel·lícules (alfabètic per títol)"
+echo "2.         Mostrar llistat de pel·lícules ordenats cronològicament per any">
+echo "3.         Mostrar llistat de pel·lícules ordenades per popularitat de més a menys">
+
+case $i in
+        1)
+                echo a:: 
+        2)
+                echo b::
+        3)
+                echo c::
+
